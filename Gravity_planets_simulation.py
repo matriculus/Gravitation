749a533557aa01b22763e816d633a0b212f0b0bf
Vishnu Pradeesh <pradeeshvishnu@outlook.com>
2018-05-25 21:15:38 +0530

    GIF added

--- Gravity_planets_simulation.py
+++ Gravity_planets_simulation.py
@@ -2,6 +2,11 @@
 import pygame
 import numpy as np
 from Gravity_assets import *
+import os
+
+directory = "Images"
+if not os.path.exists(directory):
+    os.makedirs(directory)
 
 # set up screen size
 screen_size = width, height = (1024, 720)
@@ -13,19 +18,19 @@ screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Gravity Simulation")
 
 env = Environment(screen, width, height, background_colour)
-env.addPlanets(100)
+env.addPlanets(50)
 
 screen.fill(env.colour)
 running = True
-# file_num = 0
+file_num = 0
 while running:
     screen.fill(env.colour)
     env.showPlanets()
     env.resultant_forces()
     env.update()
-    # fname = "Gravitation_{}.png".format(file_num)
-    # pygame.image.save()
-    filenum += 1
+    fname = directory + "/Gravitation_{0:04}.png".format(file_num)
+    pygame.image.save(screen, fname)
+    file_num += 1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # event.type will be QUIT when close button is pressed
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Gravitation
 
 This is a simulation of gravitational attraction between multiple objects. The planets and the environment is created in the module assets. The Planet class will take care of the movement and properties of individual planetary body and the Environment class will take care of the coupled reactions of all the bodies.
+
+![GIF](Images.gif)
